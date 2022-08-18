41faf76aa7f8674a10a315bac430915ab1e9d598
juanmanuelgalvan <juanmanuelgalvanmorales@gmail.com>
2022-08-18 00:18:28 -0500

    primer commit

---
+++ src/Data2.js
@@ -0,0 +1,127 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Puma Bratz Jade",
+      price: 401,
+      image: "https://www.youloveit.com/uploads/posts/2021-08/1628275751_youloveit_com_bratz_puma.jpg",
+      category: "Bratz Puma",
+      img1: "https://www.youloveit.com/uploads/posts/2021-08/1628275751_youloveit_com_bratz_puma.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Botas de flamas con plataforma",
+      price: 1700,
+      image: "https://img.ltwebstatic.com/images3_pi/2022/03/17/16474981557f1e6f7926a8383937ef778dd6cd56a5_thumbnail_600x.webp",
+      category: "Botas",
+      img1: "https://img.ltwebstatic.com/images3_pi/2022/03/17/16474981557f1e6f7926a8383937ef778dd6cd56a5_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Botas Negras de plataforma con Cadenas",
+      price: 475,
+      image: "https://img.ltwebstatic.com/images3_pi/2022/07/22/165845267016b9372581794b1f7c7d60576c50dd7c_thumbnail_600x.webp",
+      category: "Botas",
+      img1: "https://img.ltwebstatic.com/images3_pi/2022/07/22/165845267016b9372581794b1f7c7d60576c50dd7c_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Nike LD Waffle Sacai White Nylon",
+      price: 399,
+      image: "https://cf.shopee.com.mx/file/d61be7675c151b6c1ec03c63f1202420",
+      category: "nike",
+      img1: "https://cf.shopee.com.mx/file/d61be7675c151b6c1ec03c63f1202420",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Nike Dunk Low SP Kentucky (2021)",
+      price: 405,
+      image: "https://ae01.alicdn.com/kf/H6b42f2dfb99a43c38f35ace6866383a8w/Dark-Academia-vestido-g-tico-Vintage-de-cintura-alta-para-mujer-ropa-Sexy-con-escote-en.jpg",
+      category: "nike",
+      img1: " https://ae01.alicdn.com/kf/H6b42f2dfb99a43c38f35ace6866383a8w/Dark-Academia-vestido-g-tico-Vintage-de-cintura-alta-para-mujer-ropa-Sexy-con-escote-en.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Nike Dunk Low Off-White University",
+      price: 285,
+      image: "https://m.media-amazon.com/images/I/61nnVKchrZL._AC_SX679_.jpg",
+      category: "nike",
+      img1: "https://m.media-amazon.com/images/I/61nnVKchrZL._AC_SX679_.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Nike Air Max 2 Light Atmos",
+      price: 360,
+      image: "https://houseofrockweb.com/9791-home_default/sudadera-dark-funeral-order-of-the-black-hordes-cremallera.jpg",
+      category: "nike",
+      img1: "https://houseofrockweb.com/9791-home_default/sudadera-dark-funeral-order-of-the-black-hordes-cremallera.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Nike Air Force 1 Low CLOT Blue Silk",
+      price: 335,
+      image: "https://i.pinimg.com/originals/f3/ec/c0/f3ecc01091ea15f00501fe15a84b970b.png",
+      category: "nike",
+      img1: "https://i.pinimg.com/originals/f3/ec/c0/f3ecc01091ea15f00501fe15a84b970b.png",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Nike Air Max 90 OG Volt (2020)",
+      price: 799,
+      image: "https://http2.mlstatic.com/D_NQ_NP_776838-MLM29558567832_032019-O.webp",
+      category: "nike",
+      img1: "https://http2.mlstatic.com/D_NQ_NP_776838-MLM29558567832_032019-O.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606323250&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "Nike Dunk High Varsity Maize",
+      price: 501,
+      image: "https://m.media-amazon.com/images/I/71E-PKxOAOL._AC_SX679_.jpg",
+      category: "nike",
+      img1: "https://m.media-amazon.com/images/I/71E-PKxOAOL._AC_SX679_.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609445259&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 11,
+      title: "Nike Air Rubber Dunk Off-White UNC",
+      price: 377,
+      image: "https://img.ltwebstatic.com/images3_pi/2022/04/07/1649301943a9119839210d7c26ce9f341535cee8d9_thumbnail_600x.webp",
+      category: "nike",
+      img1: "https://img.ltwebstatic.com/images3_pi/2022/04/07/1649301943a9119839210d7c26ce9f341535cee8d9_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 12,
+      title: "Nike Air Rubber Dunk Off-White UNC",
+      price: 377,
+      image: "https://http2.mlstatic.com/D_NQ_NP_880514-MLA31653671969_082019-V.jpg",
+      category: "nike",
+      img1: "https://http2.mlstatic.com/D_NQ_NP_880514-MLA31653671969_082019-V.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
